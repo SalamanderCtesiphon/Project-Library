ddffebceb06f97c7edf19338aae9190e385ac79d
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-11-08 09:52:58 -0600

    add a function to the Object constructor

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 function Player(name, marker) {
     this.name = name;
     this.marker = marker;
+    this.sayName = function() {
+        console.log(name)
+    }
 }
 
 
-const player = new Player('steve', 'X')
-console.log(player.name);
+const player1 = new Player('steve', 'X')
+const player2 = new Player('also steve', 'O')
+player1.sayName()
+player2.sayName()
