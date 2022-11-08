fe13860e51868af47edff44718869fd28868d368
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-11-08 12:47:00 -0600

    added a function that loops through the myLibrary array and displays each book to the page

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-function Books(title, author, pages, read) {
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -8,10 +10,31 @@ function Books(title, author, pages, read) {
     }
 }
 
-const theHobbit = new Books('The Hobbit', 'J.R.R. Tolkein', "295", 'not read yet')
+function addBookToLibrary() {
+    // do stuff here
+}
+
+// a funtion to loop through the array and display each book on the page
+
+function displayBooks() {
+    let bookList = document.getElementById('book-list');
+    for (let i = 0; i < myLibrary.length; i++) {
+        let book = document.createElement('div');
+        book.classList.add('book');
+        book.innerHTML = `
+            <h3>${myLibrary[i].title}</h3>
+            <p>${myLibrary[i].author}</p>
+            <p>${myLibrary[i].pages}</p>
+            <p>${myLibrary[i].read}</p>
+        `;
+        bookList.appendChild(book);
+        }  
+}  
+
+/* const theHobbit = new Books('The Hobbit', 'J.R.R. Tolkein', "295", 'not read yet')
 
 console.log(theHobbit.info());
-
+ */
 
 
 
