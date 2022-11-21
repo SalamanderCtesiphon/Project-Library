a7ffec6f965e55172c460bd8603773bc11abfdfd
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-11-21 10:52:57 -0600

    created an object to store book details in

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+/* 
 
 let myLibrary = [];
 
@@ -54,7 +54,22 @@ function addBookToLibrary() {
 
 // a funtion to loop through the array and display each book on the page
 
+ */
 
 
 
+// write a constructor for makkng "Book" objects
 
+function Book(title, author, numberOfPages, haveRead) {
+    this.title = title
+    this.author = author
+    this.numberOfPages = numberOfPages
+    this.haveRead = haveRead
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.numberOfPages}, ${this.haveRead}.`;
+    }
+}
+
+const theHobbit = new Book('The Hobbit', 'J. R. R. Tolkein', '295 pages', "have read")
+
+console.log(theHobbit.info());
