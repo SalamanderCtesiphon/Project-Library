2fcf466933026822b9b8af2a1bcde93e15227ef3
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-11-21 13:16:24 -0600

    worked over the examples for prototypal inheritance

--- app.js
+++ app.js
@@ -1,66 +1,6 @@
-/* 
-
-let myLibrary = [];
-
-function Books(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        return (`The ${title} by ${author}, ${pages} pages, ${read}`)
-    }
-}
-
-//add an event listener to the button to add a book to the library
-const addBookButton = document.querySelector('#addBookButton');
-
-addBookButton.addEventListener('click', addBookToLibrary);
-
-// creat a form to input the book details
-
-const form = document.createElement('form');
-form.setAttribute('id', 'form');
-form.setAttribute('class', 'form');
-form.setAttribute('method', 'post');
-form.setAttribute('action', '#');
-
-// create a div to hold the form
-const formDiv = document.createElement('div');
-formDiv.setAttribute('id', 'formDiv');
-formDiv.setAttribute('class', 'formDiv');
-
-// append the form to the div
-formDiv.appendChild(form);
-
-
-
-
-// a function to add a book to the array
-
-function addBookToLibrary() {
-    const title = document.querySelector('#title').value;
-    const author = document.querySelector('#author').value;
-    const pages = document.querySelector('#pages').value;
-    const read = document.querySelector('#read').value;
-    const book = new Books(title, author, pages, read);
-    myLibrary.push(book);
-    console.log(myLibrary);
-    displayBooks();
-}
-
-
-
-
-// a funtion to loop through the array and display each book on the page
-
- */
-
-
-
 // write a constructor for makkng "Book" objects
 
-function Book(title, author, numberOfPages, haveRead) {
+/* function Book(title, author, numberOfPages, haveRead) {
     this.title = title
     this.author = author
     this.numberOfPages = numberOfPages
@@ -72,4 +12,31 @@ function Book(title, author, numberOfPages, haveRead) {
 
 const theHobbit = new Book('The Hobbit', 'J. R. R. Tolkein', '295 pages', "have read")
 
-console.log(theHobbit.info());
+console.log(theHobbit.info()); */
+
+function Student() {
+
+}
+
+Student.prototype.sayName = function() {
+    console.log(this.name)
+}
+
+function EighthGrader(name) {
+    this.name = name
+    this.grade = 8
+}
+
+EighthGrader.prototype = Object.create(Student.prototype)
+
+function NinthGrader(name) {
+    this.name = name
+    this.grade = 9
+}
+
+NinthGrader.prototype = Object.create(Student.prototype)
+
+NinthGrader.prototype.sayName = function() {console.log("HAHAHAHAHAHAHAHAHA")}
+
+const carl = new EighthGrader("carl")
+carl.sayName()
