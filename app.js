e2b5bcb1395fec38c3a463d830ab3ff6baf548f9
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-11-23 12:48:31 -0600

    worked the bugs out of the function

--- app.js
+++ app.js
@@ -16,16 +16,33 @@ const theHobbit = new Book('The Hobbit', 'J. R. R. Tolkein', '295 pages', "have 
 
 
 
-// take user input and store it in myLibrary array
+// take user input and store it in myLibrary array when thew submit button is clicked
 
 function addBookToLibrary() {
-    let title = document.querySelector('#title').value;
-    let author = document.querySelector('#author').value;
-    let numberOfPages = document.querySelector('#numberOfPages').value;
-    let haveRead = document.querySelector('#haveRead').value;
-    let book = new Book(title, author, numberOfPages, haveRead);
-    myLibrary.push(book);
-    console.log(myLibrary);
-    
+    //set event listener on the submit button
+    const addBookButton = document.querySelector('#addBookButton');
+    addBookButton.addEventListener('click', function() {
+        //get the user input
+        const title = document.querySelector('#title').value;
+        const author = document.querySelector('#author').value;
+        const numberOfPages = document.querySelector('#numberOfPages').value;
+        const haveRead = document.querySelector('#haveRead').value;
+        //create a new book object
+        const newBook = new Book(title, author, numberOfPages, haveRead);
+        //push new book object into myLibrary array
+        myLibrary.push(newBook);
+        //clear the form
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#numberOfPages').value = '';
+        document.querySelector('#haveRead').value = '';
+        //call the displayBooks function
+    });
 }
+
+
+
+//call the addBookToLibrary function
+addBookToLibrary();
+
 
