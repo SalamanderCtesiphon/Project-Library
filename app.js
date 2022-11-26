5bcaa9458639969a2526e39f9bb2e5fdd916384b
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-11-26 13:10:49 -0600

    created a function that displays the input form for a new book when clicked

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ function Book(title, author, numberOfPages, haveRead, notRead) {
 
 // prevent default form submission
 
-const form = document.querySelector('form');
+/* const form = document.querySelector('form');
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-});
+}); */
 
 // a function to add a book to the myLibrary array
 
@@ -35,8 +35,36 @@ function addBookToLibrary() {
  
 // an event listener to add a book to the myLibrary array when the form is submitted
 
-const submit = document.getElementById('submit');
-submit.addEventListener('click', addBookToLibrary);
+/* const submit = document.getElementById('submit');
+submit.addEventListener('click', addBookToLibrary); */
+
+// an event listener for the new book button that creates a form to add a new book
+
+const newBook = document.getElementById('new-book');
+newBook.addEventListener('click', function() {
+    const form = document.createElement('form');
+    form.setAttribute('id', 'form');
+    form.innerHTML = `
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title">
+        <label for="author">Author</label>
+        <input type="text" name="author" id="author">
+        <label for="pages">Number of Pages</label>
+        <input type="number" name="pages" id="pages">
+        <label for="haveRead">Have Read</label>
+        <input type="radio" name="haveRead" id="haveRead" value="have read">
+        <label for="notRead">Not Read</label>
+        <input type="radio" name="notRead" id="notRead" value="not read">
+        <input type="submit" value="Submit" id="submit">
+    `;
+    const container = document.querySelector('.container');
+    container.appendChild(form);
+    const submit = document.getElementById('submit');
+    submit.addEventListener('click', addBookToLibrary);
+}
+)
+
+
 
 
 
