ffce28be3a1183f3ab6aa85f82a0aa8cf7ee8699
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-11-26 12:24:43 -0600

    currently have the form functioning. the form adds a new book to the array of objects when the submit button is clicked

--- app.js
+++ app.js
@@ -2,17 +2,16 @@ let myLibrary = [];
 
 // write a constructor for makkng "Book" objects
 
-function Book(title, author, numberOfPages, haveRead) {
+function Book(title, author, numberOfPages, haveRead, notRead) {
     this.title = title
     this.author = author
     this.numberOfPages = numberOfPages
     this.haveRead = haveRead
+    this.notRead = notRead
     this.info = function() {
         return `${this.title} by ${this.author}, ${this.numberOfPages} pages, ${this.haveRead}.`;
     }
 }
-
-
 
 // prevent default form submission
 
@@ -20,7 +19,6 @@ const form = document.querySelector('form');
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 });
-
 
 // a function to add a book to the myLibrary array
 
@@ -30,12 +28,17 @@ function addBookToLibrary() {
     const numberOfPages = document.getElementById('pages').value;
     const haveRead = document.getElementById('haveRead').value;
     const notRead = document.getElementById('notRead').value;
-    const book = new Book(title, author, numberOfPages, haveRead);
+    const book = new Book(title, author, numberOfPages, haveRead, notRead);
     myLibrary.push(book);
     console.log(myLibrary);
 }
  
-addBookToLibrary();
+// an event listener to add a book to the myLibrary array when the form is submitted
+
+const submit = document.getElementById('submit');
+submit.addEventListener('click', addBookToLibrary);
+
+
 
 
 
