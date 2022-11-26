b3d899fdf9e73b90ecf328cd891943932dee0b15
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-11-26 12:08:43 -0600

    fixed the default status of the submit button

--- app.js
+++ app.js
@@ -15,29 +15,13 @@ function Book(title, author, numberOfPages, haveRead) {
 const theHobbit = new Book('The Hobbit', 'J. R. R. Tolkein', '295 pages', "have read")
 
 
+// prevent default form submission
 
-// take user input and store it in myLibrary array when thew submit button is clicked
+const form = document.querySelector('form');
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+});
 
-function addBookToLibrary() {
-    //set event listener on the submit button
-    const addBookButton = document.querySelector('#addBookButton');
-    addBookButton.addEventListener('click', function() {
-        //get the user input
-        const title = document.querySelector('#title').value;
-        const author = document.querySelector('#author').value;
-        const numberOfPages = document.querySelector('#numberOfPages').value;
-        const haveRead = document.querySelector('#haveRead').value;
-        //create a new book object
-        const newBook = new Book(title, author, numberOfPages, haveRead);
-        //push new book object into myLibrary array
-        myLibrary.push(newBook);
-        //clear the form
-        document.querySelector('#title').value = '';
-        document.querySelector('#author').value = '';
-        document.querySelector('#numberOfPages').value = '';
-        document.querySelector('#haveRead').value = '';
-    });
-}
 
 
 
